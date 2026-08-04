a757422871523933707b570e24b80498d27ef3a0
MonkeMountainStudios <magnus@energiretur.se>
2026-08-04 20:37:39 +0200

    Adjust wet gain and impulse response ranges

--- program.js
+++ program.js
@@ -1158,47 +1158,47 @@
     const styles = {
       reflective: {
         wordGap: 0.024, wordGapVariation: 0.016,
-        basePitch: 0.995, gain: 0.69, wet: 0.2,
+        basePitch: 0.995, gain: 0.69, wet: 0.3,
         cadence: "falling", dramaticPauseChance: 0.08, breathChance: 0.45
       },
       oracle: {
         wordGap: 0.030, wordGapVariation: 0.020,
-        basePitch: 0.988, gain: 0.72, wet: 0.250,
+        basePitch: 0.988, gain: 0.72, wet: 0.350,
         cadence: "hill", dramaticPauseChance: 0.18, breathChance: 0.42
       },
       measured: {
         wordGap: 0.020, wordGapVariation: 0.014,
-        basePitch: 1.0, gain: 0.70, wet: 0.1,
+        basePitch: 1.0, gain: 0.70, wet: 0.25,
         cadence: "falling", dramaticPauseChance: 0.05, breathChance: 0.32
       },
       solemn: {
         wordGap: 0.034, wordGapVariation: 0.022,
-        basePitch: 0.982, gain: 0.68, wet: 0.15,
+        basePitch: 0.982, gain: 0.68, wet: 0.25,
         cadence: "falling", dramaticPauseChance: 0.13, breathChance: 0.52
       },
       certain: {
         wordGap: 0.016, wordGapVariation: 0.012,
-        basePitch: 1.006, gain: 0.74, wet: 0.17,
+        basePitch: 1.006, gain: 0.74, wet: 0.27,
         cadence: "falling", dramaticPauseChance: 0.03, breathChance: 0.24
       },
       story: {
         wordGap: 0.020, wordGapVariation: 0.016,
-        basePitch: 1.002, gain: 0.70, wet: 0.1,
+        basePitch: 1.002, gain: 0.70, wet: 0.2,
         cadence: "wandering", dramaticPauseChance: 0.06, breathChance: 0.34
       },
       dreamlike: {
         wordGap: 0.032, wordGapVariation: 0.024,
-        basePitch: 1.006, gain: 0.64, wet: 0.3,
+        basePitch: 1.006, gain: 0.64, wet: 0.5,
         cadence: "wandering", dramaticPauseChance: 0.12, breathChance: 0.58
       },
       odd: {
         wordGap: 0.022, wordGapVariation: 0.020,
-        basePitch: 1.012, gain: 0.70, wet: 0.25,
+        basePitch: 1.012, gain: 0.70, wet: 0.35,
         cadence: "rising", dramaticPauseChance: 0.10, breathChance: 0.40
       },
       "self-correcting": {
         wordGap: 0.028, wordGapVariation: 0.022,
-        basePitch: 0.997, gain: 0.67, wet: 0.34,
+        basePitch: 0.997, gain: 0.67, wet: 0.54,
         cadence: "wandering", dramaticPauseChance: 0.08, breathChance: 0.55
       }
     };
@@ -1245,11 +1245,11 @@
     lowpass.type = "lowpass";
     lowpass.frequency.value = randomBetween(3900, 6900);
 
-    wet.gain.value = randomBetween(0.25, 0.5);
+    wet.gain.value = randomBetween(0.29, 0.6);
 
     convolver.buffer = makeImpulseResponse(
       context,
-      randomBetween(1.1, 3.85),
+      randomBetween(2.2, 4.85),
       randomBetween(1.4, 3.1)
     );
 
