22cce024533186bdaf0b833076a6e43bf479621a
Monkemountainstudios <magnus@energiretur.se>
2026-08-04 23:16:40 +0200

    uip

--- program.js
+++ program.js
@@ -606,11 +606,7 @@
         ...maybeFunctionAdverb(),
         pick("verb"),
         ...buildNounPhrase(),
-<<<<<<< HEAD
         pickAvailableWord("connector", ["and", "or"]),
-=======
-        pickWord("connector", "and", "or"),
->>>>>>> 14e168c4a5b7ca7e0d78606804ff3ae2a018a0fb
         ...buildNounPhrase()
       ],
       mode: randomChoice(["story", "oracle"])
@@ -751,12 +747,7 @@
   }
 
   function sentenceItWas() {
-<<<<<<< HEAD
     const subject = pickAvailableWord("pronoun", ["it", "he", "she"]);
-=======
-    const it = pickWord("pronoun", "it", "he", "she");
-    const was = pickAvailableWord("auxiliary", ["was", "is", "will"]);
->>>>>>> 14e168c4a5b7ca7e0d78606804ff3ae2a018a0fb
 
     return {
       words: [
